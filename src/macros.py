f75f34c161d22693e476a8219844cf69ba00b8c9
thom <thom@localhost.localdomain>
2018-02-16 17:06:56 -0500

    Compatibility with a yamlmacros dev branch.

--- src/macros.py
+++ src/macros.py
@@ -1,14 +1,26 @@
+import sublime
+
 from yamlmacros.lib.extend import apply
 from yamlmacros.lib.syntax import rule as _rule
 
 from yamlmacros.lib.include import include_resource
 
+from yamlmacros import process_macros
+from yamlmacros.src.util import merge
+
 import sublime
 from os import path
 
-def get_extensions(node, eval, arguments):
+def foo(resource, arguments):
+    return process_macros(
+        sublime.load_resource(resource),
+        arguments=merge(arguments, { "file_path": resource }),
+    )
+
+def get_extensions(node, loader, **rest):
+    arguments = loader.context
     return [
-        include_resource(file_path)
+        foo(file_path, arguments)
         for file_path in sublime.find_resources('*.yaml')
         if path.dirname(file_path).endswith('Packages/JSCustom/extensions')
         and arguments.get(path.splitext(path.basename(file_path))[0], None)
